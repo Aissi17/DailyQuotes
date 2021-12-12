dcfe19b743394b59cbf56452bf889018a0c1e2f4
Islam <aissiislam97@gmail.com>
2021-12-12 21:33:28 +0100

    [ADD] added option button component

---
+++ components/OptionButton.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { StyleSheet, TouchableOpacity } from 'react-native'
+import { FontAwesome5 } from '@expo/vector-icons'
+
+
+const OptionButton = ({ name }) => {
+    return (
+        <TouchableOpacity onPress={() => { }} style={styles.btnNavigation} >
+            <FontAwesome5 name={name} size={22} color='#267871' />
+        </TouchableOpacity>
+    )
+}
+
+export default OptionButton
+
+const styles = StyleSheet.create({
+
+    btnNavigation: {
+        borderWidth: 2,
+        borderColor: '#267871',
+        borderRadius: 50,
+        padding: 15
+    }
+})
