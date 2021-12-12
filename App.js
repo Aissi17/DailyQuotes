a14154c5463c3cfdf33c40532349a275834bbde3
Islam <aissiislam97@gmail.com>
2021-12-12 21:33:54 +0100

    render the first application components

--- App.js
+++ App.js
@@ -1,21 +1,109 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Screen from './components/Screen';
+
+import { FontAwesome5 } from '@expo/vector-icons'
+import OptionButton from './components/OptionButton';
+
+import axios from 'axios'
+
+
+
 
 export default function App() {
+  const [Quote, setQuote] = React.useState('Loading...')
+  const [Author, setAuthor] = React.useState('Loading...')
+
+  getRandomQuote = async () => {
+    await axios.get("https://api.quotable.io/random").then((response) => {
+      console.log(response.data);
+      setQuote(response.data.content)
+      setAuthor(response.data.author)
+    });
+  }
+
+  useEffect(() => {
+    getRandomQuote()
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Screen style={styles.container}>
+      <StatusBar style='dark' />
+      <View style={styles.innerContainer}>
+        <Text style={styles.title}>
+          Quote of the day
+        </Text>
+        <FontAwesome5 name='quote-left' style={{ fontSize: 16, marginBottom: -12 }} color='#000' />
+        <Text style={styles.textPragraph}>
+          {Quote}
+        </Text>
+        <FontAwesome5 name='quote-right' style={{ fontSize: 16, textAlign: 'right', marginTop: -20, marginBottom: 20 }} color='#000' />
+        <Text style={styles.authorName}> --- {Author} </Text>
+        <TouchableOpacity onPress={getRandomQuote} style={styles.btnStyle}>
+          <Text style={styles.btnText}>
+            New Quote
+          </Text>
+        </TouchableOpacity>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-around' }} >
+          <OptionButton name='volume-up' />
+          <OptionButton name='copy' />
+          <OptionButton name='twitter' />
+
+        </View>
+      </View>
+
+    </Screen>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#121212',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  innerContainer: {
+    width: '90%',
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    padding: 20,
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 26,
+    fontWeight: '600',
+    color: '#333',
+    marginBottom: 20
+  },
+  textPragraph: {
+    color: '#000',
+    fontSize: 16,
+    lineHeight: 26,
+    letterSpacing: 1.1,
+    fontWeight: '400',
+    textAlign: 'center',
+    marginBottom: 10,
+    paddingHorizontal: 30
+
+  },
+  btnStyle: {
+    backgroundColor: '#136a8a',
+    padding: 20,
+    borderRadius: 30,
+    marginVertical: 20
+  },
+  btnText: {
+    color: '#fff',
+    fontSize: 18,
+    textAlign: 'center'
+  },
+  authorName: {
+    textAlign: 'right',
+    fontWeight: '300',
+    fontStyle: 'italic',
+    fontSize: 16,
+    color: '#000'
+  }
 });
